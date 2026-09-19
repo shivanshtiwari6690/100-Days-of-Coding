27e3a6dc11d4b19107a45cbfd38bb83479708d31
Shivansh Tiwari <shivanshtiwari6690@gmail.com>
2026-09-19 22:48:39 +0530

    Add files via upload

---
+++ day16_31.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, binary = 0, place = 1, remainder;
+
+    printf("Enter a positive decimal number: ");
+    scanf("%d", &n);
+
+    if (n == 0)
+    {
+        printf("Binary = 0\n");
+    }
+    else
+    {
+        while (n > 0)
+        {
+            remainder = n % 2;
+            binary = binary + remainder * place;
+            place = place * 10;
+            n = n / 2;
+        }
+
+        printf("Binary = %d\n", binary);
+    }
+
+    return 0;
+}
