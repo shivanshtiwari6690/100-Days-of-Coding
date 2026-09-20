619073cd70a357476fe0c2c5972fba03a00ce7dc
Shivansh Tiwari <shivanshtiwari6690@gmail.com>
2026-09-21 00:18:26 +0530

    Add files via upload

---
+++ day18_36.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, remainder;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0)
+    {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+
+    printf("HCF (GCD) = %d\n", a);
+
+    return 0;
+}
