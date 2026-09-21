298ff53a2a87e9a8890b66a1d32cd5b655e8ed2e
Shivansh Tiwari <shivanshtiwari6690@gmail.com>
2026-09-22 02:44:45 +0530

    Add files via upload

---
+++ day23_45.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + (float)(2 * i) / (4 * i - 1);
+    }
+
+    printf("Sum of the series = %.2f\n", sum);
+
+    return 0;
+}
